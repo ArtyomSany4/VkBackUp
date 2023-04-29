e87e24ec5b655d81902d7e05906cfeac292dd092
IGNATEV_AA <Artyom-sany4@mail.ru>
2023-04-29 21:00:03 +0300

    Получаю максимальную ссыль на фотку и итем.сайзес от вк. \n Дальше надо собирать в датафрейм инфу по фотке \n (макс_фото_урл, количество лайков для имени.

--- backup_course_work.py
+++ backup_course_work.py
@@ -1,7 +1,6 @@
 import json
 import pandas as pd
 import requests as rq
-import pprint
 
 # Получаем токены из файликов
 with open('vk_token.txt', 'r') as vk_token_file:
@@ -9,8 +8,13 @@ with open('vk_token.txt', 'r') as vk_token_file:
 with open('ya_token.txt', 'r') as ya_token_file:
     ya_token = ya_token_file.read().strip()
 
+# Разобраться с токеном ВК, он, похоже, не нужен
+
+
 # Функция загрузки из ВК
-class VkApi:
+class VkApi:  
+    # Пользак должен ввести токен яндекса и ид пользака вк
+    # ya_token = input('Укажите токен с полигона Я.Диска: ')
     url = 'https://api.vk.com/method/'
     def __init__(self, vk_token):
         self.params = {
@@ -19,31 +23,26 @@ class VkApi:
             }
     
     def download(self, owner_id=None):
+        vk_photo_types = {
+            'w': 10, 'z': 9, 'y': 8, 'r': 7, 'q': 6, 'p': 5, 'o': 4, 'x':3, 'm': 2, 's': 1}        
         ava_df = pd.DataFrame()
         download_url = self.url + 'photos.get'
         download_params = {
             'owner_id': owner_id, # если не объявлен дополнительно, определит по владельцу токена
             'album_id': 'profile', # служебный альбом, только авы
             # 'photo_sizes': '', разобраться,
-            'count': 3,
+            'count': 10,
             'extended': 1            
             }
         req = rq.get(download_url, params={**self.params, **download_params}).json()
         # Цикл для перебора типов размеров фото
-        # Создать список с размерами фото из доки апи
-        # Итерироваться по элементам этого списка, элемент передавать в if ТУТЬ in el_size['type']:
-        for el in req['response']['items']: 
-            for el_size in el['sizes']:
-                print(el_size['type'])
-                if 's' in el_size['type']:
-                    print(el_size['url'])  
-                    print()
-            # print(el)
+        for item in req['response']['items']:
+            max_photo_url = max(item['sizes'], key=lambda x: vk_photo_types[x["type"]])
             
 
         ava_df = pd.DataFrame(req['response']['items'][0]['sizes'])
         # print(req)
-        return ava_df
+        # return ava_df
 
 
 
