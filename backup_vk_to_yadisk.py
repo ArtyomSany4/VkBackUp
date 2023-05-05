aab62b3a3e3db3dca12cb624ba0c260d46db06ab
IGNATEV_AA <Artyom-sany4@mail.ru>
2023-05-05 19:45:39 +0300

    Удалось подложить один файл на ядиск, но положился ссылкой. Надо разобраться с data в аплоаде

--- backup_vk_to_yadisk.py
+++ backup_vk_to_yadisk.py
@@ -12,18 +12,26 @@ with open('ya_token.txt', 'r') as ya_token_file:
 with open('vk_token.txt', 'r') as vk_token_file:
     vk_token = vk_token_file.read().strip()
     
-path_to_file = bya.YaUploader(ya_token)
-print(path_to_file.create_folder()['href'])
-print()
+# href_for_path = bya.YaUploader(ya_token)
 
-# for_ya_list = vk.VkPhotosGet(vk_token)
-# # for_ya_list.get_url()
+# Создаем папку на ядиске (если не передать имя, 
+# то в имя пихнет VK_PHOTO_BACKUP)
+folder_name = input('Введите имя папки: ')
+# path_on_yadisk = bya.YaUploader(ya_token).create_folder(yad_folder_name)
+# print(path_on_yadisk)
 
-# for el in for_ya_list.get_url():
-#     date = datetime.utcfromtimestamp(el["date"]).strftime("%Y-%m-%d %H:%M:%S")
-#     filename = f'{el["likes"]}_{date}.jpg'
-#     f_to_upload = bya.YaUploader(ya_token)
-#     # print(ya_token)
-#     print()
+# Получаем список фоток для загрузки
+for_ya_list = vk.VkPhotosGet(vk_token)
+
+# Цикл для каждой фотки
+for el in for_ya_list.get_url():
+    date = datetime.utcfromtimestamp(el["date"]).strftime("%Y-%m-%d %H_%M_%S")
+    filename = f'{el["likes"]}_05.jpg'
+    photo_url = el['url']
+    f_to_upload = bya.YaUploader(ya_token) # хз, зачем это
+    f_to_upload.upload(photo_url, filename, folder_name)
+    # print(ya_token)
+    print()
     
     
+# path_to_file, filename
--- backup_ya_api.py
+++ backup_ya_api.py
@@ -1,8 +1,9 @@
 import requests as rq
 
-# token = ''
 with open('ya_token.txt', 'r') as ya_token_file:
     ya_token = ya_token_file.read().strip()
+
+# ya_token = ''
     
 class YaUploader:
     def __init__(self, ya_token: str):
@@ -21,28 +22,29 @@ class YaUploader:
         create_resp = rq.put(create_folder_url, headers=headers, params=params)
         return create_resp.json()
     
-    def _get_upload_link(self, path_to_file):
-        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+    def get_upload_link(self, path_on_yadisk):
+        method_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {'path': path_to_file, 'overwrite': 'true'}
-        response = rq.get(upload_url, headers=headers, params=params)
+        params = {'path': path_on_yadisk, 'overwrite': 'true'}
+        response = rq.get(method_url, headers=headers, params=params)
+        # print('гет аплоад', response['href'])
         return response.json()
 
-    def upload(self, path_to_file, filename):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        href = self._get_upload_link(path_to_file=path_to_file).get('href', '')
-        response = rq.put(href, data=open(filename, 'rb'))
-        response.raise_for_status()
-        if response.status_code == 200:
-            print('Загрузка успешно завершена')
-        if response.status_code == 201:
-            print('Файл успешно отправлен на загрузку')
-
-if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = 'путь'
-    token = ya_token
-    path = YaUploader(token)
-    result = path.create_folder(path_to_file)
-    
-    # print(result)
+    def upload(self, photo_url, filename, folder_name='VK_PHOTO_BACKUP'):
+        # href = self.get_upload_link(path_to_file=path_to_file).get('href', '')
+        photo_url = photo_url
+        folder_name = folder_name
+        upload_link = self.get_upload_link(folder_name)['href']
+        print(photo_url)
+        print(folder_name)
+        print(upload_link)
+        print()
+        response = rq.put(upload_link, data=open(photo_url, 'rb'))
+        return response
+        
+        # response.raise_for_status()
+        # if response.status_code == 200:
+        #     print('Загрузка успешно завершена')
+        # if response.status_code == 201:
+        #     print('Файл успешно отправлен на загрузку')
+# print('тестовый принт', YaUploader(ya_token).create_folder())
